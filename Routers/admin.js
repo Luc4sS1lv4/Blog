270d316e22f06eec114d56fe964ff0d16e079ef9
Luc4sS1lv4 <lucas.lukas3245@gmail.com>
2024-12-18 16:56:42 -0400

    Trabalhando no edit

--- Routers/admin.js
+++ Routers/admin.js
@@ -2,54 +2,63 @@ const express = require('express')
 const router = express.Router()
 mongoose = require('mongoose')
 require('../models/Categoria')
-const categoria = mongoose.model('Categoria')
+const categoria = mongoose.model("Categoria")
 
-router.get("/posts", (req, res)=>{
+router.get("/posts", (req, res) => {
     res.render("admin/index")
 })
 
-router.get('/categorias', (req, res)=>{
+router.get('/categorias', (req, res) => {
 
-    categoria.find().then((categorias)=>{
-        res.render("admin/categorias", {categorias:categorias.map(categor => categor.toJSON())})
+    categoria.find().then((categorias) => {
+        res.render("admin/categorias", { categorias: categorias.map(categor => categor.toJSON()) })
     })
-    .catch((err)=>{
-        req.flash("error_msg", "Houve u erro ao registrar")
-        res.redirect('/admin')
-    })
+        .catch((err) => {
+            req.flash("error_msg", "Houve u erro ao registrar")
+            res.redirect('/admin')
+        })
 })
 
-router.get('/categorias/add', (req, res)=>{
+router.get('/categorias/add', (req, res) => {
     res.render('admin/addcategoria')
 })
 
-router.post("/categorias/nova", (req, res)=>{
+router.post("/categorias/nova", (req, res) => {
 
     var erros = []
 
-if(!req.body.Nome || typeof req.body.Nome == undefined || req.body.Nome == null){
-    erros.push({Texto: "Nome inválido"})
-}
-if(!req.body.Sobrenome || typeof req.body.Sobrenome == undefined || req.body.Sobrenome == null){
-    erros.push({Texto: "Sobrenome inválido"})}
-
-if(erros.length > 0){
-    res.render("admin/addcategoria", {erros: erros})
-}else{
-    const novaCategoria = {
-        Nome: req.body.Nome,
-        Sobrenome: req.body.Sobrenome
+    if (!req.body.Nome || typeof req.body.Nome == undefined || req.body.Nome == null) {
+        erros.push({ Texto: "Nome inválido" })
     }
-    new categoria(novaCategoria).save().then(()=>{
-        req.flash("success_msg", "categoria Cadastrada com sucesso")
-        res.redirect("/admin/categorias")
+    if (!req.body.Sobrenome || typeof req.body.Sobrenome == undefined || req.body.Sobrenome == null) {
+        erros.push({ Texto: "Sobrenome inválido" })
+    }
 
-    }).catch((error)=>{
-        req.flash("error_msg", "Houve um erro ao salvar, tente novamente")
-        res.redirect("admin/categorias")
-    })}
+    if (erros.length > 0) {
+        res.render("admin/addcategoria", { erros: erros })
+    } else {
+        const novaCategoria = {
+            Nome: req.body.Nome,
+            Sobrenome: req.body.Sobrenome
+        }
 
-}
-)
+        new categoria(novaCategoria).save().then(() => {
+            req.flash("success_msg", "categoria Cadastrada com sucesso")
+            res.redirect("/admin/categorias")
+
+        }).catch((error) => {
+            req.flash("error_msg", "Houve um erro ao salvar, tente novamente")
+            res.redirect("/admin/categorias")
+        })
+    }
+
+})
+
+router.get("/categorias/edit/:id", (req, res) => {
+    categoria.findOne({ _id: req.params.id }).lean().then((categoria) => {
+        res.render("admin/editeCategorias", { categoria: categoria })
+
+    })
+})
 
 module.exports = router
--- models/Categoria.js
+++ models/Categoria.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const Categoria = new Schema({
-    Nome:{
+    Nome: {
         type: String,
         require: true
     },
     Sobrenome: {
         type: String,
-        require:true
+        require: true
     },
-    Data:{
+    Data: {
         type: Date,
         default: Date.now()
     }
